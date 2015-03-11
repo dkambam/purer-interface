8000e6732c3163d80ccd3e43b8cff298f3eba945
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-10 23:04:29 -0400

    complete free-function/ 	a demo for ipplementing callback for a free-function using a function pointer

---
+++ free-function/free-function.cpp
@@ -0,0 +1,24 @@
+/*
+	Implementing callbacks for a free-function
+	Using a function-pointer
+*/
+#include <iostream>
+
+int sum(int a, int b); // returns the sum of a & b
+
+int main(){
+	
+
+	int (*callback)(int, int); 	// definition
+	callback = &sum; 			// assignment	
+	
+	int result = callback(2,3); // the call
+
+
+	std::cout << result << std::endl;
+	return 0;
+}
+
+int sum(int a, int b){
+	return a + b;
+}
