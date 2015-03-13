03d6286865a376bc2409f7c192db0709b247174a
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-13 11:47:37 -0400

    Complete unassigned-std-function/

--- dangling-function-pointer/unassigned-std-function.cpp
+++ dangling-function-pointer/unassigned-std-function.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <functional>
+
 int main(){
+
+	std::function< void(std::string) > paint; // defined but not assigned
+	paint("green"); // throws: bad_function_call
+
 	return 0;
 }
