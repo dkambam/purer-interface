dd37bc88c59eedb818f3679254e8ba7059756bd1
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-13 13:13:59 -0400

    Add code for validity tests

--- dangling-function-pointer/unassigned-std-function.cpp
+++ dangling-function-pointer/unassigned-std-function.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <functional>
+#include <exception>
+void printMsg(std::string msg);
 
 int main(){
 
 	std::function< void(std::string) > paint; // defined but not assigned
-	paint("green"); // throws: bad_function_call
+	try{
+		paint("red"); // throws: bad_function_call
+	}catch(std::bad_function_call &e){
+		std::cerr << e.what() << ": bad_function_call" << std::endl;
+	}
+
+	if(paint){	// test for validity
+		paint("green");
+	}
 
 	return 0;
+}
+
+void printMsg(std::string msg){
+	std::cout << msg << std::endl;
 }
