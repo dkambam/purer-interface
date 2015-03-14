d9a957e5d5b407f7b8b5c4ecd8984651143ea143
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-14 14:45:36 -0400

    Update stub. Add working code.

--- std-bind/args-dont-matter/args-dont-matter.cpp
+++ std-bind/args-dont-matter/args-dont-matter.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+
+int sub(int a, int b);
 
 int main(){
+
+	std::function< int(int,int) > f;
+	f = std::bind( sub, 29, 9 );
+
+	int result;
+	result = f(10,5); 	// actual-args are ignored
+						// but deos not throw an error! :)
+
+	std::cout << "result: " << result << std::endl;
+
 	return 0;
+}
+
+int sub(int a, int b){
+	int result = a - b;
+	std::cout << "sub: " << "(" << a << " - " << b << ")" ;
+	std::cout << " = " << result << std::endl;
+	return result;
 }
