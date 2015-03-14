7aa9d328c7588887c8265a585774a39f2fe16d95
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-14 01:49:20 -0400

    Update std-bin/simple-example/

--- std-bind/simple-example/std-bind.cpp
+++ std-bind/simple-example/std-bind.cpp
@@ -5,13 +5,28 @@ DEMO: a simple exable for bind
 #include <iostream>
 #include <functional>
 
+using std::placeholders::_1;
+
 int sub( int a, int b );
 
 int main(){
-	std::function< int() > f = std::bind( sub, 7, 2); // create functor using bind
-	std::cout << f() << std::endl; // call functor
+	std::function< int() > f1;
+	f1 = std::bind( sub, 7, 2); 	// bind both arguments (to objects)
+	std::cout << f1() << std::endl; // call functor
+
+
+	std::function< int(int) > f2;
+	f2 = std::bind( sub, _1, 2 ); 		// bind 1st argument to placeholder
+										// bind 2nd argument to integer (object)
+	
+	std::cout << f2(9) << std::endl;	// 9 is the first argument to sub
+
+
+
 }
 
 int sub( int a, int b ){
-	return a-b;
+	int result = a-b;
+	std::cout << "sub: (" << a << " - " << b << ") is " << result << std::endl;
+	return result;
 }
