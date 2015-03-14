864287ce617566a7a344545cc8f73f9fd54951a4
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-14 14:47:34 -0400

    Fix typo. Convert tabs --> space

--- std-bind/args-dont-matter/args-dont-matter.cpp
+++ std-bind/args-dont-matter/args-dont-matter.cpp
@@ -4,21 +4,21 @@ int sub(int a, int b);
 
 int main(){
 
-	std::function< int(int,int) > f;
-	f = std::bind( sub, 29, 9 );
+    std::function< int(int,int) > f;
+    f = std::bind( sub, 29, 9 );
 
-	int result;
-	result = f(10,5); 	// actual-args are ignored
-						// but deos not throw an error! :)
+    int result;
+    result = f(10,5);   // actual-args are ignored
+                        // but does not throw an error! :)
 
-	std::cout << "result: " << result << std::endl;
+    std::cout << "result: " << result << std::endl;
 
-	return 0;
+    return 0;
 }
 
 int sub(int a, int b){
-	int result = a - b;
-	std::cout << "sub: " << "(" << a << " - " << b << ")" ;
-	std::cout << " = " << result << std::endl;
-	return result;
+    int result = a - b;
+    std::cout << "sub: " << "(" << a << " - " << b << ")" ;
+    std::cout << " = " << result << std::endl;
+    return result;
 }
